e63e6e37e4102f8f191b53be7daf50898c707f73
Alexandra <alexandra.prisecaru@aihr.com>
2025-01-30 23:16:50 +0200

    Added unit test for largest diamond (Z)

--- DiamondLetters/DiamondLetters.UnitTests/DiamondServiceTests.cs
+++ DiamondLetters/DiamondLetters.UnitTests/DiamondServiceTests.cs
@@ -27,20 +27,20 @@ public class DiamondServiceTests
     [Fact]
     public void CreateDiamond_OnCharacterB_ReturnsDiamondThatHasBInTheMiddle()
     {
-        string bDiamond = @"
+        string expectedDiamond = @"
  A 
 B B
  A ".TrimStart('\n');
 
         string result = diamondService.GetDiamondRepresentation('B');
-        Assert.Equal(bDiamond.Length, result.Length);
-        Assert.Equal(bDiamond, result);
+        Assert.Equal(expectedDiamond.Length, result.Length);
+        Assert.Equal(expectedDiamond, result);
     }
 
     [Fact]
     public void CreateDiamond_OnCharacterC_ReturnsDiamondThatHasCInTheMiddle()
     {
-        string cDiamond = @"
+        string expectedDiamond = @"
   A  
  B B 
 C   C
@@ -48,7 +48,69 @@ C   C
   A  ".TrimStart('\n');
 
         string result = diamondService.GetDiamondRepresentation('C');
-        Assert.Equal(cDiamond.Length, result.Length);
-        Assert.Equal(cDiamond, result);
+        Assert.Equal(expectedDiamond.Length, result.Length);
+        Assert.Equal(expectedDiamond, result);
+    }
+    
+    [Fact]
+    public void CreateDiamond_OnCharacterZ_ReturnsDiamondThatHasZInTheMiddle()
+    {
+        string expectedDiamond = @"
+                         A                         
+                        B B                        
+                       C   C                       
+                      D     D                      
+                     E       E                     
+                    F         F                    
+                   G           G                   
+                  H             H                  
+                 I               I                 
+                J                 J                
+               K                   K               
+              L                     L              
+             M                       M             
+            N                         N            
+           O                           O           
+          P                             P          
+         Q                               Q         
+        R                                 R        
+       S                                   S       
+      T                                     T      
+     U                                       U     
+    V                                         V    
+   W                                           W   
+  X                                             X  
+ Y                                               Y 
+Z                                                 Z
+ Y                                               Y 
+  X                                             X  
+   W                                           W   
+    V                                         V    
+     U                                       U     
+      T                                     T      
+       S                                   S       
+        R                                 R        
+         Q                               Q         
+          P                             P          
+           O                           O           
+            N                         N            
+             M                       M             
+              L                     L              
+               K                   K               
+                J                 J                
+                 I               I                 
+                  H             H                  
+                   G           G                   
+                    F         F                    
+                     E       E                     
+                      D     D                      
+                       C   C                       
+                        B B                        
+                         A                         ".TrimStart('\n');
+
+
+        string result = diamondService.GetDiamondRepresentation('Z');
+        Assert.Equal(expectedDiamond.Length, result.Length);
+        Assert.Equal(expectedDiamond, result);
     }
 }
