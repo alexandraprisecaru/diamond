874ed9dd6f722b9d8bfa929c686a2754b7d84431
Alexandra <alexandra.prisecaru@aihr.com>
2025-01-30 22:27:09 +0200

    Simple unit test for "A" letter scenario

--- DiamondLetters/DiamondLetters.UnitTests/DiamondServiceTests.cs
+++ DiamondLetters/DiamondLetters.UnitTests/DiamondServiceTests.cs
@@ -2,11 +2,18 @@ namespace DiamondLetters.UnitTests;
 
 public class DiamondServiceTests
 {
+    readonly DiamondService diamondService = new();
+
     [Fact]
     public void CreateDiamond_OnInvalidCharacter_ThrowArgumentException()
     {
-        var diamondService = new DiamondService();
-
         Assert.Throws<ArgumentException>(() => diamondService.GetDiamondRepresentation('1'));
+    }
+
+    [Fact]
+    public void CreateDiamond_OnCharacterA_ReturnsTheSameCharacterAsString()
+    {
+        var result = diamondService.GetDiamondRepresentation('A');
+        Assert.Equal("A", result);
     }
 }
--- DiamondLetters/DiamondLetters/DiamondService.cs
+++ DiamondLetters/DiamondLetters/DiamondService.cs
@@ -2,11 +2,18 @@ namespace DiamondLetters;
 
 public class DiamondService
 {
-    public void GetDiamondRepresentation(char letter)
+    public string GetDiamondRepresentation(char letter)
     {
+        if (letter == 'A')
+        {
+            return letter.ToString();
+        }
+        
         if (!char.IsLetter(letter))
         {
             throw new ArgumentException("Character is not a letter");
         }
+
+        return null!;
     }
 }
